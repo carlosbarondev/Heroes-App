dd5c63740b73d482c0511de3eb9ea6c8bc914494
carlosbarondev <carlosbarondev@gmail.com>
2021-11-24 13:57:40 +0100

    docs actualizado

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -16,11 +16,11 @@ export const DashboardRoutes = () => {
             <div className="container">
 
                 <Routes>
-                    <Route path="/marvel" element={<MarvelScreen />} />
-                    <Route path="/dc" element={<DcScreen />} />
+                    <Route path="marvel" element={<MarvelScreen />} />
+                    <Route path="dc" element={<DcScreen />} />
 
-                    <Route path="/search" element={<SearchScreen />} />
-                    <Route path="/hero/:heroeId" element={<HeroScreen />} />
+                    <Route path="search" element={<SearchScreen />} />
+                    <Route path="hero/:heroeId" element={<HeroScreen />} />
 
                     <Route path="/" element={<MarvelScreen />} />
 
