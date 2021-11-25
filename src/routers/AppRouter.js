4896a4455b519b67747ef7ec56e67d580c86a751
carlosbarondev <barondev@gmail.com>
2021-11-25 14:32:36 +0100

    rutas privadas y públicas implementadas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,20 +1,49 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter/*, HashRouter*/ } from "react-router-dom";
+
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 
 export const AppRouter = () => {
     return (
-        <HashRouter>
+
+        // Para desplegar la aplicación en GitHub Pages
+        // <HashRouter>
+
+        <BrowserRouter>
 
             <Routes>
 
-                <Route path="/login" element={<LoginScreen />} />
+                <Route
+                    path="/login"
+                    element=
+                    {
+                        <PublicRoute>
+                            <LoginScreen />
+                        </PublicRoute>
+                    }
+                />
 
-                <Route path="/*" element={<DashboardRoutes />} />
+                {/* <Route path="/login" element={<LoginScreen />} /> */}
+
+                <Route
+                    path="/*"
+                    element=
+                    {
+                        <PrivateRoute>
+                            <DashboardRoutes />
+                        </PrivateRoute>
+                    }
+                />
+
+                {/* <Route path="/*" element={<DashboardRoutes />} /> */}
 
             </Routes>
+        </BrowserRouter>
 
-        </HashRouter>
+        // </HashRouter>
+
     )
 }
